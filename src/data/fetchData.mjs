d0e2e490b67151708f0c1344da648b901d6d2981
Cihan Erenler <cihanerenler@outlook.com>
2023-01-12 15:16:29 +0200

    implement pilot data fetching

--- src/data/fetchData.mjs
+++ src/data/fetchData.mjs
@@ -1,34 +1,29 @@
 import axios from "axios";
 
-export const fetchDroneData = async (url) => {
-  try {
-    const result = await axios(url);
-    return result;
-  } catch (error) {
-    console.log(error);
-  }
-};
+export const eventHandler = async (pilotURL, violations, socket) => {
+	try {
+		const calls = violations.map((v) => {
+			const serialNumber = v.serialNumber;
+			return axios.get(`${pilotURL}/${serialNumber}`);
+		});
 
-export const fetchPilotInfo = async (violations) => {
-  const url = `${process.env.REACT_APP_BASE_URL}/birdnest/pilots`;
-  try {
-    const calls = violations.map((v) => {
-      const serialNumber = v.serialNumber;
-      return axios.get(`${url}/${serialNumber}`);
-    });
+		axios
+			.all(calls, {
+				validateStatus: function (status) {
+					return (status >= 200 && status < 300) || status === 404;
+				},
+			})
+			.then(
+				axios.spread((...response) => {
+					const temp = response.filter((res) => {
+						if (res.data) return true;
+					});
 
-    // make multiple calls
-    axios.all(calls).then(
-      axios.spread((...response) => {
-        const tempForm = response.map((res) => res.data);
-        const finalForm = [...violatingPilots, ...tempForm];
-        let uniqueObjArray = [
-          ...new Map(finalForm.map((item) => [item["pilotId"], item])).values(),
-        ];
-        setViolatingPilots(uniqueObjArray);
-      })
-    );
-  } catch (error) {
-    throw new Error(error);
-  }
+					const pilots = temp.map((pilot) => pilot.data);
+					socket.emit("violation", pilots);
+				})
+			);
+	} catch (error) {
+		console.error(error.message);
+	}
 };
--- src/server.mjs
+++ src/server.mjs
@@ -1,52 +1,63 @@
 import express from "express";
 import http from "http";
-import { Server } from "socket.io";
-import { fetchDroneData } from "./data/fetchData.mjs";
-import { formatObject, parseXML, detectViolation } from "./helpers.mjs";
 import dotenv from "dotenv";
+import axios from "axios";
+import events from "events";
+import { Server } from "socket.io";
+import { formatObject, parseXML, detectViolation } from "./helpers.mjs";
+import { eventHandler } from "./data/fetchData.mjs";
 dotenv.config();
-
-const DELAY = 2000;
-
-// Server port number
-const port = process.env.PORT || 3002;
 
 // Base api URL
 const baseURL = process.env.BASE_URL;
-
+const pilotURL = process.env.PILOT_URL;
 // Create express app
 const app = express();
-
 // Create server
 const server = http.createServer(app);
-
 // Create socket server
 const io = new Server(server, {
-  cors: { origin: "*" },
+	cors: { origin: "*" },
 });
+
+// Global variables
+const DELAY = 2000;
+const PORT = process.env.PORT || 3002;
+let violations;
+
+// Declare event emmiter
+const eventEmmiter = new events.EventEmitter();
 
 // Fetch data every 2 seconds
 setTimeout(async function run() {
-  console.log("girdiii");
-  try {
-    const result = await fetchDroneData(baseURL);
-    // Parse XML to JS
-    const jsonData = await parseXML(result.data);
-    // Re-arrange the object shape and calculate the distance to the nest for each drone
-    const drones = formatObject(jsonData);
-    // Filter violations
-    const violations = detectViolation(drones);
-    // Publish the data to all clients
-    io.emit("drone-data", drones);
+	try {
+		const result = await axios(baseURL);
+		// Parse XML to JS
+		const jsonData = await parseXML(result.data);
+		// Re-arrange the object shape and calculate the distance to the nest for each drone
+		const drones = formatObject(jsonData);
+		// Filter violations
+		violations = detectViolation(drones);
 
-    // Call the run() again
-    setTimeout(run, DELAY);
-  } catch (error) {
-    setTimeout(run, 0);
-    throw new Error(error._events.response);
-  }
+		// if there is a violation emmit an event
+		if (violations.length > 0) {
+			eventEmmiter.emit("violations");
+		}
+
+		// Publish the data to all clients
+		io.emit("drone-data", drones);
+
+		// Call the run() again
+		setTimeout(run, DELAY);
+	} catch (error) {
+		setTimeout(run, 500);
+		console.error(error.response.statusText);
+	}
 }, DELAY);
 
-server.listen(port, () => {
-  console.log(`server listening on port ${port}...`);
+// Call the event handler when a new violation happens
+eventEmmiter.on("violations", () => eventHandler(pilotURL, violations, io));
+
+server.listen(PORT, () => {
+	console.log(`server listening on port ${PORT}...`);
 });
